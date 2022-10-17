7f255e20686c0d72515f40986fa4908346d7a256
LISA <ASUS@LAPTOP-67G15PD7>
2022-10-17 17:10:30 +0700

    menukar element index x dengan element index y

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -6,8 +6,44 @@ using System.Threading.Tasks;
 
 namespace QuickSort
 {
-    internal class Program
+    class Program
     {
+        //Array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; //number of comparasion
+        private int[] mov_count = 0; //number of data movements
+
+        //number of elements in array
+        private int n;
+
+        
+        void read()
+        {
+            while (true)
+            {
+                Console.Write(" Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+            Console.WriteLine("\n=======================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n=======================");
+
+            //get array elements
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+
+            }
+        }
+        //swaps the element at index x with the element at index y
+
         static void Main(string[] args)
         {
         }
